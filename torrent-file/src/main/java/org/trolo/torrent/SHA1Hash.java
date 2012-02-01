70012e2f18fe344896fd01854ffc42dfb07067be
Stanislav Kurilin <st.kurilin@gmail.com>
2012-02-01 22:10:18 +0200

    Value class for hashes.

---
+++ torrent-file/src/main/java/org/trolo/torrent/SHA1Hash.java
@@ -0,0 +1,20 @@
+package java.org.trolo.torrent;
+
+/**
+ * @author: Stanislav Kurilin
+ */
+public final class Sha1Hash {
+    private final String value;
+
+    public static Sha1Hash create(String hash) {
+        return new Sha1Hash(hash);
+    }
+
+    private Sha1Hash(String value) {
+        this.value = value;
+    }
+
+    public String hash() {
+        return value;
+    }
+}
