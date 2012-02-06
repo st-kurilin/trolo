28dabbae940b7bfd698f878dc815d49f31fc385c
Stanislav Kurilin <st.kurilin@gmail.com>
2012-02-06 10:38:42 +0200

    Extend ByteLists.

--- common/src/main/java/org/trolo/common/ByteLists.java
+++ common/src/main/java/org/trolo/common/ByteLists.java
@@ -3,6 +3,7 @@ package org.trolo.common;
 import com.google.common.base.Charsets;
 import com.google.common.collect.ImmutableList;
 import com.google.common.primitives.Bytes;
+import com.google.common.primitives.Ints;
 
 import java.text.DecimalFormat;
 import java.text.ParseException;
@@ -25,11 +26,20 @@ public class ByteLists {
         }
     }
 
+    public static long toLong2(List<Byte> buffer) {
+        final ImmutableList.Builder<Byte> res = ImmutableList.<Byte>builder().add((byte) 0).add((byte) 0).addAll(buffer);
+        return Ints.fromByteArray(Bytes.toArray(res.build()));
+    }
+
     public static ImmutableList<Byte> fromLong(long value) {
         return fromString(Long.toString(value));
     }
 
     public static ImmutableList<Byte> fromString(String val) {
         return ImmutableList.copyOf(Bytes.asList(val.getBytes(Charsets.UTF_8)));
+    }
+
+    public static ImmutableList<Byte> fromBytes(byte[] value) {
+        return ImmutableList.copyOf(Bytes.asList(value));
     }
 }
