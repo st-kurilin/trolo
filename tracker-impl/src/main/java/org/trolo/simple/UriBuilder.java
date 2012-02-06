31290eaadb1cd3d90229afb7ee02c46904c6ba25
Stanislav Kurilin <st.kurilin@gmail.com>
2012-02-06 10:37:52 +0200

    UtlBuilder fix.

--- tracker-impl/src/main/java/org/trolo/simple/UriBuilder.java
+++ tracker-impl/src/main/java/org/trolo/simple/UriBuilder.java
@@ -1,20 +1,21 @@
 package org.trolo.simple;
 
-import com.google.common.base.Charsets;
 import com.google.common.base.Optional;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import org.trolo.common.ByteLists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URLEncoder;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * @author Stanislav Kurilin
  */
 class UriBuilder {
+    private static Logger LOG = LoggerFactory.getLogger(TrackerImpl.class);
     private final URI base;
     private final ImmutableMap.Builder<String, String> queryBuilder = ImmutableMap.builder();
 
@@ -23,23 +24,18 @@ class UriBuilder {
     }
 
 
-    public UriBuilder add(String key, ImmutableList<Byte> value) {
-        try {
-            queryBuilder.put(
-                    URLEncoder.encode(key, Charsets.UTF_8.displayName()),
-                    URLEncoder.encode(ByteLists.toString(value), Charsets.UTF_8.displayName()));
-            return this;
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
-        }
+    public UriBuilder add(String key, List<Byte> value) {
+        queryBuilder.put(key, urlEncode(value));
+        return this;
     }
 
     public UriBuilder add(String key, String value) {
-        return add(key, ByteLists.fromString(value));
+        queryBuilder.put(key, value);
+        return this;
     }
 
     public UriBuilder add(String key, long value) {
-        return add(key, ByteLists.fromLong(value));
+        return add(key, Long.toString(value));
     }
 
     public URI build() {
@@ -47,12 +43,28 @@ class UriBuilder {
         final StringBuilder query = new StringBuilder();
         query.append(Optional.fromNullable(base.getQuery()).or(""));
         for (String key : additionalQuery.keySet()) {
-            query.append(key).append("=").append(additionalQuery.get(key));
+            query.append(key).append("=").append(additionalQuery.get(key)).append("&");
         }
+        LOG.debug("Additional query {}", query);
         try {
-            return new URI(base.getScheme(), base.getAuthority(), base.getPath(), query.toString(), base.getFragment());
+            return new URI(String.format("%s://%s%s?%s", checkNotNull(base.getScheme()), checkNotNull(base.getAuthority()), checkNotNull(base.getPath()), query.toString()));
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static String urlEncode(List<Byte> bs) {
+
+        //URLEncoder.encode(ByteLists.toString(bs), Charsets.UTF_8.displayName()))
+        final StringBuilder sb = new StringBuilder(bs.size() * 3);
+        for (byte b : bs) {
+            int c = b & 0xFF;
+            sb.append('%');
+            if (c < 16)
+                sb.append('0');
+            sb.append(Integer.toHexString(c));
+        }
+        LOG.debug("UrlEncode in:{};out:{}", bs, sb);
+        return sb.toString();
     }
 }
